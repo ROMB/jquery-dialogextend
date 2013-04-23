0e1430934d84bb79d5eac4e512e0f8887424520b
psmak4 <psmak4@gmail.com>
2013-04-23 17:55:38 -0300

    Add "ui-state-default" class
    Added "ui-state-default" class to buttons which places the light gray box on the background to match the close button.

--- jquery.dialogextend.1_0_1.js
+++ jquery.dialogextend.1_0_1.js
@@ -287,9 +287,9 @@
 				.end();
 			// append other buttons to button-pane
 			$(buttonPane)
-				.append('<a class="ui-dialog-titlebar-maximize ui-corner-all" href="#"><span class="ui-icon '+settings.icons.maximize+'">maximize</span></a>')
-				.append('<a class="ui-dialog-titlebar-restore ui-corner-all" href="#"><span class="ui-icon '+settings.icons.restore+'">restore</span></a>')
-				.append('<a class="ui-dialog-titlebar-minimize ui-corner-all" href="#"><span class="ui-icon '+settings.icons.minimize+'">minimize</span></a>')
+				.append('<a class="ui-dialog-titlebar-maximize ui-corner-all ui-state-default" href="#"><span class="ui-icon '+settings.icons.maximize+'">maximize</span></a>')
+				.append('<a class="ui-dialog-titlebar-restore ui-corner-all ui-state-default" href="#"><span class="ui-icon '+settings.icons.restore+'">restore</span></a>')
+				.append('<a class="ui-dialog-titlebar-minimize ui-corner-all ui-state-default" href="#"><span class="ui-icon '+settings.icons.minimize+'">minimize</span></a>')
 				// add effect to buttons
 				.find(".ui-dialog-titlebar-maximize,.ui-dialog-titlebar-minimize,.ui-dialog-titlebar-restore")
 					.attr("role", "button")
